f9fb2b52978f20fedfc5a4920a0ff428d7685386
Abdelrahman Mohamed Mostafa <a.m.mostafa1@student.aast.edu>
2023-04-13 14:53:55 +0200

    make titles dynamic

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#titles", {
+  strings: ["Computer Engineering Student", "Data Analyist"],
+  startDelay: 100,
+  typeSpeed: 150,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+  showCursor: false,
+});
